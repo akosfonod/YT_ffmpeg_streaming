15968d19941ca6c2a7f0b53314a01147b9ed7825
Akos Fonod - Acer <akosfonod@gmail.com>
2019-06-19 21:13:55 +0200

    Adding SPI OLED python script. Sources are from: https://github.com/adafruit/Adafruit_Python_SSD1306/blob/master/examples/animate.py

---
+++ OLED-screen-driver.py
@@ -0,0 +1,42 @@
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
+
+# Raspberry Pi pin configuration:
+RST = 24
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# 128x64 display with hardware SPI:
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+
+# Initialize library.
+disp.begin()
+
+# Get display width and height.
+width = disp.width
+height = disp.height
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create image buffer.
+# Make sure to create image with mode '1' for 1-bit color.
+image = Image.new('1', (width, height))
+
+# Load default font.
+font = ImageFont.load_default()
+
+# Create drawing object.
+draw = ImageDraw.Draw(image)
+
+# Define text and get total width.
+text = 'SSD1306 ORGANIC LED DISPLAY. THIS IS AN OLD SCHOOL DEMO SCROLLER!! GREETZ TO: LADYADA & THE ADAFRUIT CREW, TRIXTER, FUTURE CREW, AND FARBRAUSCH'
+maxwidth, unused = draw.textsize(text, font=font)
